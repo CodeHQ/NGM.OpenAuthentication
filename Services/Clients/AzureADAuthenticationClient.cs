ce77fcee253e3e79fcd4c0db59380ea369a213e8
Craig Pretorius <craigp@codehq.co.za>
2015-04-01 18:28:44 +0200

    Change Google OpenId to Google OAuth 2 client

--- Services/Clients/AzureADAuthenticationClient.cs
+++ Services/Clients/AzureADAuthenticationClient.cs
@@ -2,13 +2,17 @@ using DotNetOpenAuth.AspNet;
 using DotNetOpenAuth.AspNet.Clients;
 using NGM.OpenAuthentication.Models;
 
-namespace NGM.OpenAuthentication.Services.Clients {
-    public class AzureADAuthenticationClient : IExternalAuthenticationClient {
-        public string ProviderName {
+namespace NGM.OpenAuthentication.Services.Clients
+{
+    public class AzureADAuthenticationClient : IExternalAuthenticationClient
+    {
+        public string ProviderName
+        {
             get { return "AzureAD"; }
         }
 
-        public IAuthenticationClient Build(ProviderConfigurationRecord providerConfigurationRecord) {
+        public IAuthenticationClient Build(ProviderConfigurationRecord providerConfigurationRecord)
+        {
             return new AzureADClient(providerConfigurationRecord.ProviderIdKey, providerConfigurationRecord.ProviderSecret);
         }
     }
---
+++ Services/Clients/GoogleAuthenticationClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using DotNetOpenAuth.AspNet;
+using DotNetOpenAuth.AspNet.Clients;
+using DotNetOpenAuth.GoogleOAuth2;
+using NGM.OpenAuthentication.Models;
+
+namespace NGM.OpenAuthentication.Services.Clients
+{
+    public class GoogleAuthenticationClient : IExternalAuthenticationClient
+    {
+        public string ProviderName
+        {
+            get { return "Google"; }
+        }
+
+        public IAuthenticationClient Build(ProviderConfigurationRecord providerConfigurationRecord) {
+            var client = new GoogleOAuth2Client(providerConfigurationRecord.ProviderIdKey, providerConfigurationRecord.ProviderSecret);
+
+            return client;
+        }
+    }
+}
